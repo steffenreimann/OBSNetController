a4a4cda417fa97af3866fdedf95d1f647851c95a
Steffens-WinTower <steffenreimann2011@googlemail.com>
2018-11-02 03:19:49 +0100

    -

--- release-builds/OBSNetControl-darwin-x64/OBSNetControl.app/Contents/Resources/app/preload.js
+++
@@ -1,5 +0,0 @@
-document.addEventListener("DOMNodeInserted", function(event) {
-    if (!!window && !(!!window.$)) {
-        window.$ = window.jQuery = require('path/to/jquery.js');
-    }
-});
--- release-builds/OBSNetControl-darwin-x64/OBSNetControl.app/Contents/Resources/app/test.js
+++
@@ -1,5 +0,0 @@
-var easymidi = require('easymidi');
-var input = new easymidi.Input('Akai APC20');
-input.on('noteon', function (msg) {
-  // do something with msg
-});
